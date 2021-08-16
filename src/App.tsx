0d0abf093591811ada1262ee3ffc89b07277436c
Arun Singh <arunsingh28aug.as@gmail.com>
2021-08-17 00:19:40 +0530

    topic respons

--- src/App.tsx
+++ src/App.tsx
@@ -15,8 +15,8 @@ function App() {
       <div className="App">
         <Navbar />
         <Hero />
-         {/* <Topics />
-        <Section3 />
+        <Topics />
+        {/*<Section3 />
         <Quality />
         <Testimonial /> */}
       </div>
