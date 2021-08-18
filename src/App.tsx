67551f5edbe47a67c85d547f5667d910557d29f5
Arun Singh <arunsingh28aug.as@gmail.com>
2021-08-19 01:10:29 +0530

    for mobile

--- src/App.tsx
+++ src/App.tsx
@@ -12,15 +12,25 @@ import { Footer } from './components/Footer';
 function App() {
   return (
     <>
-      <div className="App">
-        <Navbar />
-        <Hero />
-        <Topics />
-        <Section3 />
-        {/*<Quality />
-        <Testimonial /> */}
+      <div className="appWrapper">
+        <div className="App">
+          <Navbar />
+          <Hero />
+          <Topics />
+          <Section3 />
+          <Quality />
+          <Testimonial />
+        </div>
+        <Footer />
+        {/* message for small screen */}
       </div>
-      {/* <Footer /> */}
+      <div className="message">
+        <h3>Your device size is not suitable for this website please open it on large screen device (desktop/laptop)<span>BTW hi</span></h3>
+        <div className="footer">
+          <span>Design & code by </span>
+          <a href="https://www.google.com/search?q=arunsingh28&rlz=1C1RXQR_enIN965IN965&sxsrf=ALeKk00wQ_D7lgL0O6whYTOwv-VCeTS1Pw%3A1629314302968&ei=_lwdYZbUOtqsrQGFhKqwAQ&oq=arun&gs_lcp=Cgdnd3Mtd2l6EAMYADIECCMQJzIECCMQJzIECCMQJzIKCC4QsQMQgwEQQzILCC4QgAQQsQMQgwEyBAgAEEMyBAgAEEMyCwgAEIAEELEDEIMBMggILhCABBCxAzILCC4QgAQQsQMQgwE6EQguEIAEELEDEIMBEMcBENEDOgUIABCRAjoFCC4QgARKBAhBGABQgM8mWI3TJmD62iZoAHACeACAAZ4CiAGkB5IBBTAuNC4xmAEAoAEBwAEB&sclient=gws-wiz"><span>Arun Singh </span>🦄</a>
+        </div>
+      </div>
     </>
   );
 }
